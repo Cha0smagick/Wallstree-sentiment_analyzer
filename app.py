519f11bf6249a0866090143d1dadadc4fe484066
Cha0smagick <alekos20@gmail.com>
2023-12-30 15:03:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import requests
 import time
 
-# Function to fetch top 50 stocks discussed on Wallstreetbets
-def get_reddit_stocks(date=None, max_retries=3):
+# Function to fetch top 25 stocks discussed on Wallstreetbets
+def get_reddit_stocks(date=None, max_retries=3, limit=25):
     url = 'https://tradestie.com/api/v1/apps/reddit'
     if date:
         url += f'?date={date}'
@@ -15,7 +15,7 @@ def get_reddit_stocks(date=None, max_retries=3):
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
             data = response.json()
-            return data
+            return data[:limit]  # Limit results to the top 25
         elif response.status_code == 429:
             st.warning(f"Rate limit exceeded. Retrying in 5 seconds. Retry attempt: {retries + 1}/{max_retries}")
             time.sleep(5)
@@ -27,8 +27,8 @@ def get_reddit_stocks(date=None, max_retries=3):
     st.error(f"Reached the maximum number of retries. Unable to fetch Reddit stocks.")
     return None
 
-# Function to fetch TTM Squeeze stocks
-def get_ttm_squeeze_stocks(date=None):
+# Function to fetch top 25 TTM Squeeze stocks
+def get_ttm_squeeze_stocks(date=None, limit=25):
     if not date:
         st.warning("Please enter a date before fetching TTM Squeeze stocks.")
         return None
@@ -39,7 +39,7 @@ def get_ttm_squeeze_stocks(date=None):
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
         data = response.json()
-        return data
+        return data[:limit]  # Limit results to the top 25
     else:
         st.error(f"Error fetching TTM Squeeze stocks. Status code: {response.status_code}")
         return None
@@ -51,10 +51,10 @@ st.title('Wallstreetbets & TTM Squeeze Stocks')
 selected_date = st.text_input('Enter date (yyyy-mm-dd):', '')
 
 # Button to fetch and display Reddit stocks
-if st.button('Fetch Top 50 Reddit Stocks'):
+if st.button('Fetch Top 25 Reddit Stocks'):
     reddit_stocks = get_reddit_stocks(selected_date)
     if reddit_stocks is not None:
-        st.header('Top 50 Stocks Discussed on Wallstreetbets')
+        st.header('Top 25 Stocks Discussed on Wallstreetbets')
         with st.container():
             for stock in reddit_stocks:
                 st.table({
@@ -64,10 +64,10 @@ if st.button('Fetch Top 50 Reddit Stocks'):
                 })
 
 # Button to fetch and display TTM Squeeze stocks
-if st.button('Fetch TTM Squeeze Stocks for Date'):
+if st.button('Fetch Top 25 TTM Squeeze Stocks for Date'):
     ttm_squeeze_stocks = get_ttm_squeeze_stocks(selected_date)
     if ttm_squeeze_stocks is not None:
-        st.header('TTM Squeeze Stocks')
+        st.header('Top 25 TTM Squeeze Stocks')
         with st.container():
             for stock in ttm_squeeze_stocks:
                 st.table({
